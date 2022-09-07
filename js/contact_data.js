c06cc1a8a9456890a28c96058934f6374e70c3ac
Halex000 <hclipton@hotmail.com>
2022-09-06 23:34:50 -0400

    updated the contact-sheet using a bootstrapTable data for table querried from contact_data.js

---
+++ js/contact_data.js
@@ -0,0 +1,12 @@
+var contacts = `[
+    {
+        "Name": "Harry Lipton",
+        "E_mail": "Example@gmail.com",
+        "Phone_Number": "(111) 111-1111"
+    },
+    {
+        "Name": "Cameron Parrish",
+        "E_mail": "Another_Example@gmail.com",
+        "Phone_Number": "(222) 222-2222"
+    }
+]`;
