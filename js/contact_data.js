7f2fffdcbae05152f02fa623661a63a7b1741854
SmokeIce593 <37118809+SmokeIce593@users.noreply.github.com>
2022-09-08 12:09:31 -0400

    Ryan forgot to commit

--- js/contact_data.js
+++ js/contact_data.js
@@ -1,12 +1,71 @@
-var contacts = `[
-    {
-        "Name": "Harry Lipton",
-        "E_mail": "Example@gmail.com",
-        "Phone_Number": "(111) 111-1111"
-    },
-    {
-        "Name": "Cameron Parrish",
-        "E_mail": "Another_Example@gmail.com",
-        "Phone_Number": "(222) 222-2222"
-    }
-]`;
+const USER_ID = sessionStorage.getItem('userID');
+const urlBase = 'http://67.207.82.94/LAMPAPI';
+const extension = 'php';
+
+// Creates a row
+function createRow(name, email, phone) {
+    return `<tr>
+    <td>${name}</td>
+    <td>${email}</td>
+    <td>${phone}</td>
+    <td>
+      <span class="material-icons">edit</span>
+      <span class="material-icons">delete</span>
+    </td>
+    </tr>`;
+}
+
+// Adds an entry to the database
+function addEntry() {
+    const newContact = {
+        firstname: $('#addModalFirstName').val(),
+        lastname: $('#addModalLastName').val(),
+        phone: $('#addModalPhone').val(),
+        address: $('#addModalEmail').val(),
+        userId: USER_ID,
+    };
+    console.log(newContact);
+    doContact(newContact);
+    $(".add-modal").css("display", "none");
+}
+
+function showAddModal() {
+    $(".add-modal").css("display", "initial");
+}
+// Deletes all ten rows
+function deleteRows() {
+    $('#tableBody').empty();
+}
+
+function doContact(newContact)
+{
+	let jsonPayload = JSON.stringify( newContact );
+
+	let url = urlBase + '/AddContactAPI.' + extension;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200 && xhr.responseText)
+			{
+				let jsonObject = JSON.parse( xhr.responseText );
+                console.log(jsonObject);
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		console.log(err);
+	}
+}
+
+// Create empty rows
+for (let i = 0; i < 10; i++) {
+    $("#tableBody").append(createRow("","",""));
+}
+
